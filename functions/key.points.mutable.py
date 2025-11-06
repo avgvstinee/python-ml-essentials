41135a5242c3b5c1c3e9b31bb976cb9d41a26a11
codewithsyre <augustineshoks7@gmail.com>
2025-11-06 09:24:21 +0200

    Changing a mutable object

---
+++ functions/key.points.mutable.py
@@ -0,0 +1,10 @@
+
+# Changing a mutable Object 
+
+values = [1,2,3]
+
+def func (values):
+    values[1] = 42 # This affects the 'values' argument 
+
+func(values)
+print(values)  # Output: [1, 42, 3]
