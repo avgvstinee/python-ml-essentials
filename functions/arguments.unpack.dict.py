cce076cd34a19182265cf237e284099443da25c9
codewithsyre <augustineshoks7@gmail.com>
2025-11-06 11:32:59 +0200

    Dictionary unpacking

--- functions/arguments.unpack.dict.py
+++ functions/arguments.unpack.dict.py
@@ -1,1 +1,8 @@
 
+# Dictionary unpacking
+# syntax : **dict_name to unpack the key-value pairs    
+
+def personInfo(name, age, city):
+    print(name, age, city)
+info = {"name": "Kholofelo", "age": 25, "city": "Johannesburg"}
+personInfo(**info)  # Output: Kholofelo 25 Johannesburg
