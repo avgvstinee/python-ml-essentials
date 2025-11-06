62e5ca2d61aa50a620353d51b8cf0597ebb1256b
codewithsyre <augustineshoks7@gmail.com>
2025-11-06 10:44:11 +0200

    positional arguments

---
+++ functions/arguments.positional.py
@@ -0,0 +1,7 @@
+
+# positional arguments
+
+def fullName(firstName, middleName,lastName):
+    print(firstName, middleName, lastName)
+
+fullName("Kholofelo", "Augustine", "Shokane") # Output: Kholofelo Augustine Shokane
