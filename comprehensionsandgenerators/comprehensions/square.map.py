e5ec0622240666d6464fa55607c57a4875c88801
codewithsyre <augustineshoks7@gmail.com>
2025-12-07 18:08:34 +0200

    pythonic list

---
+++ comprehensionsandgenerators/comprehensions/square.map.py
@@ -0,0 +1,13 @@
+
+# lst method 
+square = []
+
+for n in range(10):
+    square.append(n ** 2)
+    
+print(square)  # Output: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+# 2nd method 
+
+squares = list(map(lambda x: x ** 2, range(10)))
+print(squares)  # Output: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
