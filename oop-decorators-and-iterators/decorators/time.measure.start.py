6b3bd2546b9bf6e043cf03a7db8a1458274a092b
codewithsyre <augustineshoks7@gmail.com>
2026-05-19 22:23:08 +0200

    decorators start

---
+++ oop-decorators-and-iterators/decorators/time.measure.start.py
@@ -0,0 +1,19 @@
+from time import time , sleep
+
+def f():
+    sleep(.3)
+
+def g():
+    sleep(.5)
+    
+# measure time taken by f and g
+
+t = time()
+# call
+f()
+print('f took', time() - t) # f took 0.3050727844238281
+
+t = time()
+# call
+g()
+print('g took', time() - t) # g took 0.5009047985076904
