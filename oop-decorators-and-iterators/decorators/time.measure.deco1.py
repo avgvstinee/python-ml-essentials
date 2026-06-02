8e6656ead13dcc26d7683a4c16ac3b1424b1d43c
codewithsyre <augustineshoks7@gmail.com>
2026-06-02 13:46:09 +0200

    decorators decoration

---
+++ oop-decorators-and-iterators/decorators/time.measure.deco1.py
@@ -0,0 +1,18 @@
+
+from time import time, sleep
+
+def f (sleep_time = 0.1):
+    sleep(sleep_time)
+
+def measure(func):
+    def wrapper(*args , **kwargs):
+        t = time()
+        func(*args , **kwargs)
+        print(func.__name__ , 'took' , time() - t)
+    return wrapper
+
+f = measure(f) # decoration point 
+
+f(sleep_time = 0.3)
+f(0.2)
+print(f.__name__) # wrapper <-- Ouch!
