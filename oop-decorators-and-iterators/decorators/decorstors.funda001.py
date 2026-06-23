a87f0306c6b0474b2451fe336a35d069c84cca16
codewithsyre <augustineshoks7@gmail.com>
2026-06-23 17:51:02 +0200

    functions fundamentals

---
+++ oop-decorators-and-iterators/decorators/decorstors.funda001.py
@@ -0,0 +1,10 @@
+
+
+def outer(x): # x=5
+    def inner(y):  # y=10
+        return x + y
+    return inner
+
+add_five = outer(5)
+result = add_five(10) # inner(10) => 5 + 10 = 15
+print(f'x + y : {result}') 
