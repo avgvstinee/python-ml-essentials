4b7283a592e09422d9519d7de038a13e93be582f
codewithsyre <augustineshoks7@gmail.com>
2026-06-25 20:20:36 +0200

    improved inheritance

---
+++ oop-decorators-and-iterators/oop/inheritance/super.explicit.py
@@ -0,0 +1,13 @@
+
+class Book :
+    def __init__(self,title,publisher,pages):
+        self.title = title
+        self.publisher = publisher
+        self.pages = pages
+
+class Ebook(Book):
+    def __init__(self,title,publisher,pages,format_):
+        Book.__init__(self,title,publisher,pages)
+        self.format_ = format_
+
+ebook = Ebook('Learn Python Programming")
